a48d7cdee031246573c74908cc02f7b7c4789992
Sarah <Sarah>
2018-05-19 03:42:44 +0200

    Initial Commit

---
+++ StanfordCourse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StanfordCourse
+//
+//  Created by Sarah Gamal on 5/19/18.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
